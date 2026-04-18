7914cc0a0265ff725abbfad7c1a5f0fc570454b7
polorky <65815313+polorky@users.noreply.github.com>
2026-04-18 21:45:29 +0100

    URLs update

--- league/urls.py
+++ league/urls.py
@@ -3,14 +3,6 @@ from . import views
 from league.utilities.player import verify_player
 
 urlpatterns = [
-<<<<<<< HEAD
-=======
-    #path('nominations/<str:pagename>', views.nominations, name='nominations'),
-
-
-    path('clubadmin/<str:update>', views.clubadmin, name='clubadmin'),
-
->>>>>>> e4f0df6de8da34f9cbb8b6e2dad315165dff4045
     path("divisions/<str:pagename>/<str:season>", views.DivisionsView.as_view(), name='divisions'),
     path("divisions/<str:pagename>", views.DivisionsView.as_view(), name='divisions'),
     path('clubs/<str:pagename>', views.ClubsView.as_view(), name='clubs'),
@@ -19,15 +11,6 @@ urlpatterns = [
     path('playerstats/<str:pagename>', views.PlayerStatsView.as_view(), name='playerstats'),
     path('player/<str:playerid>/<str:from>', views.PlayerView.as_view(), name='player'),
     path('archive/<str:pagename>', views.ArchivesView.as_view(), name='archive'),
-<<<<<<< HEAD
-=======
-
-    path('clubadmin/', views.clubadmin, name='clubadmin'),
-    path('admin/league/', views.LeagueAdminView.as_view(), name='league_admin'),
-    path('admin/website/', views.WebsiteAdminView.as_view(), name='website_admin'),
-    path('admin/club/', views.ClubAdminView.as_view(), name='club_admin'),
-
->>>>>>> e4f0df6de8da34f9cbb8b6e2dad315165dff4045
     path('juniors', views.JuniorsView.as_view(), name='juniors'),
     path('help', views.HelpView.as_view(), name='help'),
     path('stats', views.StatsView.as_view(), name='stats'),
